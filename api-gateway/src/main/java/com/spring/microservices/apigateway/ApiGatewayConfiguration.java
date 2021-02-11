ad812703c3e23cf7ce6681ce55d9bf7b442a99e3
sruthiviswanathan <sravan1097@gmail.com>
2021-02-11 17:50:09 +0530

    Added Spring cloud gateway logging filter.

--- api-gateway/src/main/java/com/spring/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/spring/microservices/apigateway/ApiGatewayConfiguration.java
@@ -19,6 +19,9 @@ public class ApiGatewayConfiguration {
                 .route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
                 .route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
                 .route(p -> p.path("/currency-conversion-feign/**").uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}")).uri("lb://currency-conversion"))
                 .build();
     }
 }
