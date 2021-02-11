d71a06be64f8e480318ff225ac0b68f0dfff6604
sruthiviswanathan <sravan1097@gmail.com>
2021-02-11 18:58:23 +0530

    Added circuitBreaker, ratelimiter and bulkhead configs.

--- currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,10 +1,12 @@
 package com.spring.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +18,10 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+//    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+    @RateLimiter(name = "default") // In 10s => 10000 calls to sample api
+    @Bulkhead(name = "default")
     public String sampleApi() {
         logger.info("Sample Api call received");
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/get", String.class);
