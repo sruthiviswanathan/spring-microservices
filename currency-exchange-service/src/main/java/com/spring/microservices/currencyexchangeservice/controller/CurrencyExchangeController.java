13f639c82466046963d5318c70ae780fb9ab12ec
sruthiviswanathan <sravan1097@gmail.com>
2021-02-12 14:01:35 +0530

    Added zipkin to all the microservices to enable distributed tracing.

--- currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.spring.microservices.currencyexchangeservice.controller;
 
 import com.spring.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.spring.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -19,6 +23,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{fromValue}/to/{toValue}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String fromValue, @PathVariable String toValue) {
+        logger.info("Retrieve exchange method called with "+ fromValue + " and " + toValue);
         CurrencyExchange value = currencyExchangeRepository.findByFromAndTo(fromValue, toValue);
         if (value == null) {
             throw new RuntimeException("Unable to find currency exchange data");
